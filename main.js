3f75a6358d20a9a21c502f44cd9b419a1e782e2f
ChernobilskyFox <55699743+ChernobilskyFox@users.noreply.github.com>
2019-09-24 15:39:23 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const score = document.querySelector('.score'),
     start = document.querySelector('.start'),
-    gameArea = document.querySelector('gameArea');
+    gameArea = document.querySelector('.gameArea'),
+    car = document.createElement('div');
+car.classList.add('car');
 
 start.addEventListener('click', startGame);
 document.addEventListener('keydown', startRun);
@@ -13,17 +15,70 @@ const keys = {
     ArrowLeft: false
 };
 
-function startRun(){
-    Console.log('start');
+const setting = {
+    start: false,
+    score: 0,
+    speed: 3
+};
+
+function startGame(){
+    start.classList.add('hide');
+
+    for (let i = 0; i < 20; i++){
+        const line = document.createElement('div');
+        line.classList.add('line');
+        line.style.top = (i * 100) + 'px';
+        line.y = i * 100;
+        gameArea.appendChild(line);
+    }
+    setting.start = true;
+    gameArea.appendChild(car);
+    setting.x = car.offsetLeft;
+    setting.y = car.offsetTop;
+    requestAnimationFrame(playGame);
 }
 
-function stopRun(event){
+function playGame(){
+    console.log('Play GAME!');
+    moveRoad();
+    if(setting.start){
+        if(keys.ArrowLeft && setting.x > 0){
+            setting.x -= setting.speed;
+        }
+        if(keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)){
+            setting.x += setting.speed;
+        }
+        if(keys.ArrowUp && setting.y > 0){
+            setting.y -= setting.speed;
+        }
+        if(keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)){
+            setting.y += setting.speed;
+        }
+
+        car.style.left = setting.x +'px';
+        car.style.top = setting.y +'px';
+
+        requestAnimationFrame(playGame);
+    }
+} 
+
+function startRun(event){
     event.preventDefault();
     keys[event.key] = true;
 }
 
-function startGame(event){
+function stopRun(event){
     event.preventDefault();
-    start.classList.add('hide');
+    keys[event.key] = false;
 }
 
+function moveRoad() {
+    let lines = document.querySelectorAll('.line');
+    lines.forEach(function(line){
+        line.y += setting.speed;
+        line.style.top = line.y +'px';
+        if(line.y >= document.documentElement.clientHeight){
+            line.y = -100;
+        }
+    })
+}
